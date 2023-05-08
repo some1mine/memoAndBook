7b89263af5f500f9ec2a16bafd53f8bdffaec6f3
zhu <parkjw573@naver.com>
2023-05-09 06:17:16 +0900

    initial commit

---
+++ src/test/java/com/example/memoandbook/MemoAndBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memoandbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoAndBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
